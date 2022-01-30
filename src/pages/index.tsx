9610de20ef0909cda32bd9730f45fac20f4b7504
IgorMichael <igormichael120@gmail.com>
2022-01-30 02:35:13 -0300

    Creating all pages statically

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
+import { Button, Flex, Stack } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import { Input } from '../components/Form/Input';
 
-const Home: NextPage = () => {
-  return <h1>Dashgo</h1>;
+const SignIn: NextPage = () => {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack>
+          <Input name="email" type="email" label="E-mail" />
+
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 };
 
-export default Home;
+export default SignIn;
