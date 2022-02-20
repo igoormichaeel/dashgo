a3fa46eecfac28fb786897e9ef42e10a800027d8
IgorMichael <igormichael120@gmail.com>
2022-02-20 02:59:16 -0300

    MirageJS and react-query configured for user listing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,29 @@
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from '../styles/theme';
-
+import { makeServer } from '../services/mirage';
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { SideBarDrawerProvider } from '../context/SidebarDrawerContext';
+
+import { theme } from '../styles/theme';
+import { ChakraProvider } from '@chakra-ui/react';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
+
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SideBarDrawerProvider>
-        <Component {...pageProps} />
-      </SideBarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SideBarDrawerProvider>
+          <Component {...pageProps} />
+        </SideBarDrawerProvider>
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,7 +5,7 @@ import {
   Flex,
   Heading,
   Icon,
-  IconButton,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -15,14 +15,51 @@ import {
   Tr,
   useBreakpointValue,
 } from '@chakra-ui/react';
+import { AiOutlineReload } from 'react-icons/ai';
+import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+
 import Head from 'next/head';
 import Link from 'next/link';
-import { RiAddLine, RiPencilLine } from 'react-icons/ri';
+
+import { api } from '../../services/api';
+import { useQuery } from 'react-query';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+};
+
 export default function UserList() {
+  const { data, isLoading, isFetching, error, refetch } = useQuery<User[]>(
+    'users',
+    async () => {
+      const { data } = await api.get('users');
+
+      const users = data.users.map((user: User) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString('pt-br', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric',
+          }),
+        };
+      });
+
+      return users;
+    },
+    {
+      staleTime: 1000 * 5, // 5 seconds
+    }
+  );
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -43,141 +80,95 @@ export default function UserList() {
             <Flex mb="8" justify="space-between" align="center">
               <Heading size="lg" fontWeight="normal">
                 Usuários
+                {!isLoading && isFetching && (
+                  <Spinner size="sm" color="gray.500" ml="4" />
+                )}
               </Heading>
 
-              <Link href="/users/create" passHref>
+              <Flex>
                 <Button
                   as="a"
                   size="sm"
                   fontSize="sm"
-                  colorScheme="pink"
+                  mr="4"
+                  colorScheme="purple"
                   _hover={{ cursor: 'pointer' }}
-                  leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                  leftIcon={<Icon as={AiOutlineReload} fontSize="16" />}
+                  onClick={() => refetch()}
                 >
-                  Criar Novo
+                  Atualizar
                 </Button>
-              </Link>
+
+                <Link href="/users/create" passHref>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="pink"
+                    _hover={{ cursor: 'pointer' }}
+                    leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                  >
+                    Criar Novo
+                  </Button>
+                </Link>
+              </Flex>
             </Flex>
 
-            <Table colorScheme="whiteAlpha">
-              <Thead>
-                <Th px={['1', '1', '6']} color="gray.300" w="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th w="8"></Th>
-              </Thead>
+            {isLoading ? (
+              <Flex justify="center">
+                <Spinner />
+              </Flex>
+            ) : error ? (
+              <Flex justify="center">
+                <Text>Falha ao obter dados dos usuários.</Text>
+              </Flex>
+            ) : (
+              <>
+                <Table colorScheme="whiteAlpha">
+                  <Thead>
+                    <Th px={['1', '1', '6']} color="gray.300" w="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th w="8"></Th>
+                  </Thead>
 
-              <Tbody>
-                <Tr>
-                  <Td px={['1', '1', '6']}>
-                    <Checkbox colorScheme="pink" />
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Igor Michael</Text>
-                      <Text fontSize="sm" color="gray.300">
-                        igormichael120@gmail.com
-                      </Text>
-                    </Box>
-                  </Td>
-                  {isWideVersion && <Td>04 de Janeiro, 2022</Td>}
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      _hover={{ cursor: 'pointer' }}
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      {isWideVersion && 'Editar'}
-                    </Button>
-                  </Td>
-                </Tr>
-                <Tr>
-                  <Td px={['1', '1', '6']}>
-                    <Checkbox colorScheme="pink" />
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Igor Michael</Text>
-                      <Text fontSize="sm" color="gray.300">
-                        igormichael120@gmail.com
-                      </Text>
-                    </Box>
-                  </Td>
-                  {isWideVersion && <Td>04 de Janeiro, 2022</Td>}
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      _hover={{ cursor: 'pointer' }}
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      {isWideVersion && 'Editar'}
-                    </Button>
-                  </Td>
-                </Tr>
-                <Tr>
-                  <Td px={['1', '1', '6']}>
-                    <Checkbox colorScheme="pink" />
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Igor Michael</Text>
-                      <Text fontSize="sm" color="gray.300">
-                        igormichael120@gmail.com
-                      </Text>
-                    </Box>
-                  </Td>
-                  {isWideVersion && <Td>04 de Janeiro, 2022</Td>}
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      _hover={{ cursor: 'pointer' }}
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      {isWideVersion && 'Editar'}
-                    </Button>
-                  </Td>
-                </Tr>
-                <Tr>
-                  <Td px={['1', '1', '6']}>
-                    <Checkbox colorScheme="pink" />
-                  </Td>
-                  <Td>
-                    <Box>
-                      <Text fontWeight="bold">Igor Michael</Text>
-                      <Text fontSize="sm" color="gray.300">
-                        igormichael120@gmail.com
-                      </Text>
-                    </Box>
-                  </Td>
-                  {isWideVersion && <Td>04 de Janeiro, 2022</Td>}
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="purple"
-                      _hover={{ cursor: 'pointer' }}
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      {isWideVersion && 'Editar'}
-                    </Button>
-                  </Td>
-                </Tr>
-              </Tbody>
-            </Table>
+                  <Tbody>
+                    {data?.map((user) => (
+                      <Tr key={user.id}>
+                        <Td px={['1', '1', '6']}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                        <Td>
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            colorScheme="purple"
+                            _hover={{ cursor: 'pointer' }}
+                            leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                          >
+                            {isWideVersion && 'Editar'}
+                          </Button>
+                        </Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                </Table>
 
-            <Pagination />
+                <Pagination />
+              </>
+            )}
           </Box>
         </Flex>
       </Box>
